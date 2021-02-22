03a7594d300528d38aa5d5b317fdefb4e4bd4cb3
RyanCrasta <65001186+RyanCrasta@users.noreply.github.com>
2021-02-22 19:37:13 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -67,7 +67,7 @@ function loadLabeledImages() {
         labels.map(async (label)=>{
             const descriptions = []
             for(let i=1; i<=2; i++) {
-                const img = await faceapi.fetchImage(`../labeled_images/${label}/${i}.jpg`)
+                const img = await faceapi.fetchImage(`./labeled_images/${label}/${i}.jpg`)
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
                 console.log(label + i + JSON.stringify(detections))
                 descriptions.push(detections.descriptor)
